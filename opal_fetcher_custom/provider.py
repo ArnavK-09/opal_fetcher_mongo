568ef120086cd91defbdc5a650bfe3489c68c0a8
Arnav K <arnavkaushal09@gmail.com>
2024-06-13 14:23:18 +0000

    base

--- opal_fetcher_custom/provider.py
+++ opal_fetcher_custom/provider.py
@@ -1,64 +1,113 @@
-# imports 
-from pydantic import List, Any, Field
+# Imports 
+from typing import Optional, List
+from pydantic import BaseModel, Field
+import motor.motor_asyncio
 from opal_common.fetcher.fetch_provider import BaseFetchProvider
 from opal_common.fetcher.events import FetcherConfig, FetchEvent
+from opal_common.logger import logger
 
+# Configuration for MongoDB connection
+class MongoConnectionParams(BaseModel):
+    """_summary_
 
-class CustomFetcherConfig(FetcherConfig):
+    Args:
+        BaseModel (_type_): _description_
+    """    
     """
-    Config for CustomFetchProvider, inherits from `FetcherConfig`.
+    MongoDB connection parameters....
     """
-    fetcher: str = "CustomFetchProvider"
-    query: str = Field(..., description="the query to search in order to fetch the data")
+    uri: str = Field(..., description="MongoDB connection string")
+    database: str = Field(..., description="Database name")
+    collection: str = Field(..., description="Collection name")
+    query: dict = Field(default={}, description="Query to fetch data")
+    fetch_one: bool = Field(False, description="Fetch only one document")
 
+# Configuration for the MongoDB Fetcher
+class MongoFetcherConfig(FetcherConfig):
+    """
+    Config for MongoFetchProvider, instance of `FetcherConfig`.
+    """
+    fetcher: str = "MongoFetchProvider"
+    connection_params: Optional[MongoConnectionParams] = Field(
+        ..., description="MongoDB connection parameters for fetcher..."
+    )
 
-class CustomFetchEvent(FetchEvent):
+# Event shape for the MongoDB Fetch Provider
+class MongoFetchEvent(FetchEvent):
     """
-    When writing a custom provider, you must create a custom FetchEvent subclass.
+    A FetchEvent shape for the MongoDB Fetch Provider.
     """
-    fetcher: str = "CustomFetchProvider"
-    config: CustomFetcherConfig = None
+    fetcher: str = "MongoFetchProvider"
+    config: MongoFetcherConfig = None
 
-class CustomFetchProvider(BaseFetchProvider):
-    """
-    The fetch-provider logic should be logically here.
-    """
-
-    def __init__(self, event: CustomFetchEvent) -> None:
+# MongoDB Fetch Provider implementation
+class MongoFetchProvider(BaseFetchProvider):
+    def __init__(self, event: MongoFetchEvent) -> None:
         """
-        Initialize your fetch provider
-        """
+        Args:
+            event (MongoFetchEvent): _description_
+        """        
+        if event.config is None:
+            event.config = MongoFetcherConfig()
         super().__init__(event)
+        self.client = motor.motor_asyncio.AsyncIOMotorClient(event.config.connection_params.uri)
+        self.collection = self.client[event.config.connection_params.database][event.config.connection_params.collection]
 
-    def parse_event(self, event: FetchEvent) -> CustomFetchEvent:
+    def parse_event(self, event: FetchEvent) -> MongoFetchEvent:
         """
-        Deserializes fetch event type from the general `FetchEvent` to your derived fetch event.
-        """
-        return CustomFetchEvent(**event.dict(exclude={"config"}), config=event.config)
+        Args:
+            event (FetchEvent): _description_
 
-    # if you require context to cleanup or guard resources, you can use __aenter__() and __aexit__()
-    async def __aenter__(self): ...
-    async def __aexit__(self, exc_type=None, exc_val=None, tb=None): ...
+        Returns:
+            MongoFetchEvent: _description_
+        """        
+        return MongoFetchEvent(**event.dict(exclude={"config"}), config=event.config)
+
+    async def __aenter__(self):      
+        self._event: MongoFetchEvent  # type casting
+        return self
+
+    async def __aexit__(self, exc_type=None, exc_val=None, tb=None):
+        """
+        Args:
+            exc_type (_type_, optional): _description_. Defaults to None.
+            exc_val (_type_, optional): _description_. Defaults to None.
+            tb (_type_, optional): _description_. Defaults to None.
+        """        
+        self.client.close()
 
     async def _fetch_(self):
-        """
-        The actual logic that you need to implement to fetch.
-        """
-        user_query: str = self._event.config.query
-        self._event: CustomFetchEvent # type cast
+        self._event: MongoFetchEvent  # type casting
 
-        return True # returning fetched results
+        if self._event.config is None:
+            logger.warning(
+                "incomplete fetcher config: MongoDB data entries require a query to specify what data to fetch!"
+            )
+            return
 
-    async def _process_(self, data: List[Any]):
-        """
-        Optional processing of the data returned by _fetch_().
-        must return json object
-        (e.g: a list or a dict that contains only serializable objects).
-        """
-        self._event: CustomFetchEvent # type cast
+        logger.debug(f"{self.__class__.__name__} fetching from {self._event.config.connection_params.uri}")
 
-        if data:
-                # we changing the datato dict that we can be later serialized to json.
-            return [dict(d) for d in data]
+        if self._event.config.connection_params.fetch_one:
+            document = await self.collection.find_one(self._event.config.connection_params.query)
+            return [document] if document else []
         else:
-            return {}
+            cursor = self.collection.find(self._event.config.connection_params.query)
+            documents = await cursor.to_list(length=None)
+            return documents
+
+    async def _process_(self, records: List[dict]):
+        """
+        Args:
+            records (List[dict]): _description_
+        """        
+        self._event: MongoFetchEvent  # type casting
+
+        # when fetch_one is true, we want to return a dict (and not a list)
+        if self._event.config.connection_params.fetch_one:
+            if records and len(records) > 0:
+                return records[0]
+            else:
+                return {}
+        else:
+            return records
+
